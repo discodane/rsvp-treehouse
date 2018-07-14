397f212ab347b7249d8703fb0b5e4a562167a8cf
discodane <el.dane.erickson@gmail.com>
2018-07-14 08:16:47 -0500

    More refactoring: Changed file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import Header from './Header';
-import MainContent from './MainContent';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 class App extends Component {
 
